5be1b91e5b777c0d6655560fba8ea3e39701be8c
Bharath-L-code <bharathl2002.xworkz@gmail.com>
2024-07-22 18:15:28 +0530

    one folder deletedd

--- d3/Camera.java
+++
@@ -1,9 +0,0 @@
-class Camera{
-	public static void main(){
-		System.out.println("Running capture in camera");
-	}
-	public static void focus(){
-		System.out.println("Running focus in camera");
-		
-	}
-}
