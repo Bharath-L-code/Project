d05236b9f75a773ae75f6c97b5383f0c6ed92550
Bharath-L-code <bharathl2002.xworkz@gmail.com>
2024-07-22 18:11:30 +0530

    new folder added

---
+++ d3/Camera.java
@@ -0,0 +1,9 @@
+class Camera{
+	public static void main(){
+		System.out.println("Running capture in camera");
+	}
+	public static void focus(){
+		System.out.println("Running focus in camera");
+		
+	}
+}
---
+++ d3/Fridge.java
@@ -0,0 +1,17 @@
+class Fridge{
+	public static void main(String[] args){
+		System.out.println("storing items in Fridge");
+		icecube();
+		vegeis();
+	}
+	public static void icecube(){
+		System.out.println("making ice cubes");
+	}
+	public static void vegeis(){
+		System.out.println("keep vegies in good condition");
+		
+	}
+	public static void cooler(){
+		System.out.println("keep the coool");
+	}
+}// i have written three static mtds here and invoked 2 in the main method answer 2 
